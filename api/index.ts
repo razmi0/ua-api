4637b346bd38e931222752fc1e0e59ace0445f14
razmiO <thomas.cuesta31@gmail.com>
2024-10-06 17:00:50 +0200

    Add /gen endpoint & length param

--- api/index.ts
+++ api/index.ts
@@ -1,14 +1,28 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
+import { Hono } from "hono";
+import { prettyJSON } from "hono/pretty-json";
+import { handle } from "hono/vercel";
+import { BASE_PATH, DEFAULT_LENGTH, VERSION } from "./const";
+import raw from "./data/uas.json";
+import { generator } from "./generator";
+import { Uas } from "./types";
+
+const { data } = raw as Uas;
+const allAgents = Object.values(data).flat();
 
 export const config = {
-  runtime: 'edge'
-}
+  runtime: "edge",
+};
 
-const app = new Hono().basePath('/api')
+const app = new Hono().basePath(`${BASE_PATH}/${VERSION}`);
 
-app.get('/', (c) => {
-  return c.json({ message: 'Hello Hono!' })
-})
+app.use(prettyJSON());
 
-export default handle(app)
+app.get("/gen", (c) => {
+  return c.json(generator(allAgents, Number(c.req.query().length) || DEFAULT_LENGTH));
+});
+
+app.get("/parser", (c) => {
+  return c.json({ message: "coucou" });
+});
+
+export default handle(app);
