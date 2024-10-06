604167f44754d59c8705d9cab8478e17d4c46fdf
razmiO <thomas.cuesta31@gmail.com>
2024-10-06 21:38:14 +0200

    Replace interfaces by types

--- api/types.ts
+++ api/types.ts
@@ -1,55 +1,55 @@
-export interface Uas {
+export type Uas = {
   data: Data;
-}
+};
 
-export interface Data {
+export type Data = {
   desktop: Desktop[];
   mobile: Mobile[];
   tablet: Mobile[];
-}
+};
 
-export interface Desktop {
+export type Desktop = {
   ua: string;
   type: DeviceInfo;
   browser: Browser;
   os: OS;
   device: Device;
-}
+};
 
-export interface Browser {
+export type Browser = {
   name: BrowserName;
   version_major: number;
   version: string;
-}
+};
 
 export type BrowserName = "Android" | "Chrome" | "Chrome Mobile" | "Firefox" | "Firefox Mobile" | "IE";
 
-export interface Device {
+export type Device = {
   name: string;
   brand: null | string;
   model: null | string;
-}
+};
 
-export interface OS {
+export type OS = {
   name: OSName;
   version_major: number | null | string;
   version: null | string;
-}
+};
 
 export type OSName = "Linux" | "Mac os X" | "Ubuntu" | "Windows";
 
-export interface DeviceInfo {
+export type DeviceInfo = {
   mobile: boolean;
   tablet: boolean;
   touch_capable: boolean;
   pc: boolean;
   bot: boolean;
-}
+};
 
-export interface Mobile {
+export type Mobile = {
   ua: string;
   type: DeviceInfo;
   browser: Browser;
   os: Browser;
   device: Device;
-}
+};
