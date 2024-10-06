9774ce74585ffcde9b2a59e197e79113cbe264a2
razmiO <thomas.cuesta31@gmail.com>
2024-10-06 21:36:57 +0200

    First fn layout

---
+++ api/parser.ts
@@ -0,0 +1,12 @@
+// const getBrowserName => (xxx) => (name: BrowserName)
+// const getBrowserVersion => (xxx) => (version: string)
+// const getBrowserVersionMajor => (xxx) => (version_major: number)
+// const getOSName => (xxx) => (name: OSName)
+// const getOSVersion => (xxx) => (version: string | null)
+// const getOSVersionMajor => (xxx) => (version_major: number | null | string)
+// const getDeviceName => (xxx) => (name: string)
+// const getDeviceBrand => (xxx) => (brand: null | string)
+// const getDeviceModel => (xxx) => (model: null | string)
+
+// const buildUA = (xxx) => string
+// const getDeviceInfo => (xxx) => (mobile: boolean, tablet: boolean, touch_capable: boolean, pc: boolean, bot: boolean)
